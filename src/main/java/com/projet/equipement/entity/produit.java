6b243e427a9e1d74eb1b6549fa104c02a14b65cf
TH-GROUP\amengue <amengue@thermador-groupe.fr>
2024-11-18 08:31:58 +0100

    init

---
+++ src/main/java/com/projet/equipement/entity/produit.java
@@ -0,0 +1,27 @@
+package com.projet.equipement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class produit {
+    @Id
+    @Column(unique=true, nullable=false, length=10)
+    private Long id;
+    @Column(nullable=false, length=100 )
+    private String name;
+    private String description;
+    private String image;
+    private String category;
+    private String price;
+    private String quantity;
+    private String unit;
+    private String status;
+    private String type;
+    private String location;
+
+
+}
