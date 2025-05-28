fc9a7ba3a260097c6d97fddfe6bdc4eeee96152d
TH-GROUP\amengue <amengue@thermador-groupe.fr>
2025-05-29 00:07:02 +0200

    Add pagination and product-specific retrieval for sales entities
    Updated APIs in `UniteVenteController` and `FormatVenteController` to support pagination. Added functionality to retrieve `FormatVente` entities by product ID. Introduced `deleteUniteVente` endpoint and adjusted service and repository layers accordingly.

--- src/main/java/com/projet/equipement/repository/FormatVenteRepository.java
+++ src/main/java/com/projet/equipement/repository/FormatVenteRepository.java
@@ -1,13 +1,18 @@
 package com.projet.equipement.repository;
 
 import com.projet.equipement.entity.FormatVente;
+import com.projet.equipement.entity.Produit;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FormatVenteRepository extends JpaRepository<FormatVente, Long> {
     @Override
     Page<FormatVente> findAll(Pageable pageable);
+
+    @Query("select f from FormatVente f where f.produit.id=:produitId")
+    Page<FormatVente> findByProduitId(Long produitId, Pageable pageable);
 }
